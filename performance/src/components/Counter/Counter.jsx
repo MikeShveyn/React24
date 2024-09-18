8cc058d8c6568492c209421eeba6bd880e9a52dc
Michael <misha.shveynfurt@gmail.com>
2024-09-18 22:50:29 +0300

    add ueMemo

--- performance/src/components/Counter/Counter.jsx
+++ performance/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -6,7 +6,7 @@ import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
 
-function isPrime(number) {
+ function isPrime(number) {
   log(
     'Calculating if is prime number',
     2,
@@ -29,7 +29,8 @@ function isPrime(number) {
 // could be removed memo cause not to mutch executions
 const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]) 
 
   const [counter, setCounter] = useState(initialCount);
 
